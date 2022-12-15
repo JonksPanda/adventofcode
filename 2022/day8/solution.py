e1dc178feb3b650d31f399b807424a4f2cf54422
jonkspanda <jionte@gmail.com>
2022-12-15 21:41:02 +0100

    Started on day9

--- 2022/day8/solution.py
+++ 2022/day8/solution.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from math import prod
 
 
 class Tree:
@@ -6,6 +7,7 @@ class Tree:
         self.size = size
         self.pos = pos
         self.visible = False
+        self.view_score = 0
 
     def compare_size(self, map):
         visible = False
@@ -43,6 +45,78 @@ class Tree:
 
         return visible
 
+    def get_view_score(self, map):
+        score = []
+
+        x = self.pos[0]
+        y = self.pos[1]
+
+        # check upper first:
+        temp_score = 0
+        highest = 0
+        for i in range(y-1, -1, -1):
+            # if map[i][x].size > highest:
+            #     highest = map[y][i].size
+            # elif map[i][x].size < highest:
+            #     break
+            if map[i][x].size < self.size:
+                temp_score += 1
+            elif map[i][x].size >= self.size:
+                temp_score += 1
+                break
+        if temp_score > 0:
+            score.append(temp_score)
+        # check down
+        temp_score = 0
+        highest = 0
+        for i in range(y+1, len(map)):
+            # if map[i][x].size > highest:
+            #     highest = map[y][i].size
+            # elif map[i][x].size < highest:
+            #     break
+            if map[i][x].size < self.size:
+                temp_score += 1
+            elif map[i][x].size >= self.size:
+                temp_score += 1
+                break
+        if temp_score > 0:
+            score.append(temp_score)
+        # check left
+        temp_score = 0
+        highest = 0
+        for row in map:
+            for i in range(x-1, -1, -1):
+                # if map[y][i].size > highest:
+                #     highest = map[y][i].size
+                # elif map[y][i].size < highest:
+                #     break
+                if map[y][i].size < self.size:
+                    temp_score += 1
+                elif map[y][i].size >= self.size:
+                    temp_score += 1
+                    break
+            if temp_score > 0:
+                score.append(temp_score)
+            break
+        # check right
+        temp_score = 0
+        highest = 0
+        for row in map:
+            for i in range(x+1, len(row)):
+                # if map[y][i].size > highest:
+                #     highest = map[y][i].size
+                # elif map[y][i].size < highest:
+                #     break
+                if map[y][i].size < self.size:
+                    temp_score += 1
+                elif map[y][i].size >= self.size:
+                    temp_score += 1
+                    break
+            if temp_score > 0:
+                score.append(temp_score)
+            break
+        return prod(score)
+
 
 def solution1(rows):
     map = []
@@ -79,9 +153,32 @@ def solution1(rows):
 
 
 def solution2(rows):
-    for row in rows:
-        pass
-    print(f"solution 2: 0")
+    map = []
+    y = 0
+
+    for tree_row in rows:
+        row = []
+        x = 0
+        for tree_size in tree_row:
+            row.append(Tree(size=int(tree_size), pos=(x, y)))
+            x += 1
+        y += 1
+        map.append(row)
+
+    scores = []
+    for row in map:
+        for tree in row:
+            if tree.pos[1] == 0 or tree.pos[1] == (len(map) - 1):
+                tree.visible = True
+            elif tree.pos[0] == 0 or tree.pos[0] == (len(row) - 1):
+                tree.visible = True
+            elif tree.size > 0:
+                tree.visible = tree.compare_size(map)
+            if tree.pos[0] > 0 and tree.pos[0] < (len(row) - 1) and tree.pos[1] > 0 and tree.pos[1] < (len(map) - 1):
+                tree.view_score = tree.get_view_score(map)
+                scores.append(tree.view_score)
+
+    print(f"solution 2: {max(scores)}")
 
 
 def read_file(filename):
--- 2022/day9/solution.py
+++ 2022/day9/solution.py
@@ -1,6 +1,46 @@
+def adjacent(head, tail):
+    # checks if on top
+    if (tail[0], tail[1]) == (head[0], head[1]):
+        return True
+    # checks if adjacent left or right
+    elif tail[0] == (head[0] + 1) or tail[0] == (head[0] - 1):
+        return True
+    # checks if adjacent down or up
+    elif tail[1] == (head[1] + 1) or tail[1] == (head[1] - 1):
+        return True
+    # checks if upper left/right
+    elif (tail[1] == (head[1] + 1) and tail[0] == (head[0] + 1)) or (tail[1] == (head[1] + 1) and tail[0] == (head[0] - 1)):
+        return True
+    # check if lower left/right
+    # elif :
+    #     pass
+    else:
+        return False
+
+
 def solution1(rows):
+    starting_pos = [0, 0]
+    head = starting_pos.copy()
+    tail = starting_pos.copy()
+    unique_positions = []
     for row in rows:
-        pass
+        direction, steps = row.split(" ")
+        steps = int(steps)
+        if direction == "D":
+            head[1] += steps
+        elif direction == "U":
+            head[1] -= steps
+        elif direction == "R":
+            head[0] += steps
+        elif direction == "L":
+            head[0] -= steps
+
+        print(adjacent(head, tail))
+
+        pos_tail = (tail[0], tail[1])
+        if (pos_tail) not in unique_positions:
+            print(pos_tail)
+            unique_positions.append(pos_tail)
     return 0
 
 
