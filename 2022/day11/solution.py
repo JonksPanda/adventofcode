aa1b191889e836766c7d0107cc9147e6b5dea86d
JonksPanda <jionte@gmail.com>
2022-12-19 23:33:38 +0100

    started on solution for day11

--- 2022/day11/solution.py
+++ 2022/day11/solution.py
@@ -1,6 +1,39 @@
+class Monkey:
+    def __init__(self, starting_items: list, operation, test, if_true, if_false):
+        self.starting_items = starting_items
+        self.operation = operation
+        self.test = test
+        self.if_true = if_true
+        self.if_false = if_false
+
+
 def solution1(rows):
+    monkeys = []
+    starting_items = []
+    operation = str()
+    test = str()
+    if_true = int()
+    if_false = int()
     for row in rows:
-        pass
+        if row.split(': ')[0] == "Starting items":
+            items = row.split(': ')[1].replace(',', '')
+            starting_items.extend(items.split(' '))
+        elif row.split(': ')[0] == "Operation":
+            operation = row.split(': ')[1].replace("new = ", "")
+        elif row.split(': ')[0] == "Test":
+            test = int(row.split(': ')[1].replace("divisible by ", ""))
+        elif row.split(': ')[0] == "If true":
+            if_true = int(row.split(': ')[1].replace("throw to monkey ", ''))
+        elif row.split(': ')[0] == "If false":
+            if_false = int(row.split(': ')[1].replace("throw to monkey ", ''))
+            monkeys.append(
+                Monkey(starting_items, operation, test, if_true, if_false))
+            # resets variables
+            starting_items = []
+            operation = str()
+            test = str()
+            if_true = int()
+            if_false = int()
     return 0
 
 
